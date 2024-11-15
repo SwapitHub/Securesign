4db424c3a83f8a6177a53d1458b8bc53a1872788
shamsher singh <shamshersingh8574@gmail.com>
2024-11-15 18:06:44 +0530

    added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import { Dashboard } from "./component/Dashboard";
+import DiamondList from "./component/DiamondList";
 
 export default function Home() {
   return (
    <>
    <Dashboard/>
+   {/* <DiamondList/> */}
    </>
   );
 }
